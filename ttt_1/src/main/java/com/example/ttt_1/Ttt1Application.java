5c8178ed79b063b569decb8d05a57e3dd5609862
yujiebin <yujiebin@4399.com>
2025-04-27 19:31:22 +0800

    feat:commit

---
+++ ttt_1/src/main/java/com/example/ttt_1/Ttt1Application.java
@@ -0,0 +1,13 @@
+package com.example.ttt_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ttt1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ttt1Application.class, args);
+    }
+
+}
